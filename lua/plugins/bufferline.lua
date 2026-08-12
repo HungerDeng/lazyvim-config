153d1b5c73dfa77ce3bdf18a55e72d362f5f589c
hungerdeng <hungerdeng@gmail.com>
2026-08-12 16:25:10 +0800

    feat(bufferline): add move and bulk-close buffer keymaps
    `Space bh`/`bl` move the current buffer left or right, and `Space bH`/`bL`
    close all buffers to the left or right.
    
    The new `Space bl` (move right) replaces LazyVim's default `Space bl`
    (close left); closing to the left is now `Space bH`.

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "<leader>bh", "<cmd>BufferLineMovePrev<cr>", desc = "Move Buffer to the Left" },
+      { "<leader>bl", "<cmd>BufferLineMoveNext<cr>", desc = "Move Buffer to the Right" },
+      { "<leader>bH", "<cmd>BufferLineCloseLeft<cr>", desc = "Delete Buffers to the Left" },
+      { "<leader>bL", "<cmd>BufferLineCloseRight<cr>", desc = "Delete Buffers to the Right" },
+    },
+  },
+}
