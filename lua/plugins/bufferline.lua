eeeedd3ae29ab51da0ad6c6acbabf466ea67737a
hungerdeng <hungerdeng@gmail.com>
2026-08-13 17:03:54 +0800

    fix(bufferline): drop redundant close-right keybinding
    `Space br` no longer closes buffers to the right. `Space bL` is now the
    single delete-right action, removing the duplicate mapping for the same
    command.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,6 +6,7 @@ return {
       { "<leader>bl", "<cmd>BufferLineMoveNext<cr>", desc = "Move Buffer to the Right" },
       { "<leader>bH", "<cmd>BufferLineCloseLeft<cr>", desc = "Delete Buffers to the Left" },
       { "<leader>bL", "<cmd>BufferLineCloseRight<cr>", desc = "Delete Buffers to the Right" },
+      { "<leader>br", false }, -- disable LazyVim's delete-right
     },
   },
 }
