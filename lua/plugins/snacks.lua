f51c50cc78abf762b76f107966cac4b3ee6983e1
hungerdeng <hungerdeng@gmail.com>
2026-09-17 10:27:35 +0800

    feat: customize the selectedLineBgColor for the lazygit inside snacks.nvim, which is very useful for unstaged-staged hunks panel

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      lazygit = {
+        config = {
+          gui = {
+            theme = {
+              selectedLineBgColor = {
+                "#e5e7eb",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
